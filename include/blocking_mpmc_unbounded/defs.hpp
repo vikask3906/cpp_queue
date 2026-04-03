6758996d47d7bfb3263dec335e36dc1dcce4138e
vikask3906 <vikaskathuria.06@gmail.com>
2026-04-04 02:34:29 +0530

    pahla commit

--- include/blocking_mpmc_unbounded/defs.hpp
+++ include/blocking_mpmc_unbounded/defs.hpp
@@ -17,64 +17,48 @@ template <typename T> class blocking_mpmc_unbounded {
   // some neutral element OR a false boolean whichever is applicable. Pop works
   // by returning the data stored in head node and replacing head to its next
   // node. We handle the empty queue gracefully as per the pop type.
+  static_assert(std::is_move_constructible_v<T>,
+                "T must be move constructible");
+
 private:
   using node = tsfqueue::__utils::Node<T>;
 
-  // Add private members :
-  // std::mutex head_mutex;
-  // std::unique_ptr<node> head;
-  // std::mutex tail_mutex;
-  // node *tail;
-  // std::condition_variable cond;
+  // Private members
+  std::mutex head_mutex;
+  std::unique_ptr<node> head;
+  std::mutex tail_mutex;
+  node *tail;
+  std::condition_variable cond;
 
-  // Description of private members :
-  // 1. std::mutex head_mutex is used to prevent contention at the head pointer
-  // This mutex is acquired when you are modifying std::unique_ptr<node> head to
-  // prevent data race.
-
-  // 2. std::unique_ptr<node> head is for the head pointer. We are using
-  // unique_ptr because this will ensure they are deleted automatically and we
-  // need not call delete manually. Also see the Node we use from utils have
-  // std::unique_ptr<Node<T>> as the next pointers which forms a chain of
-  // automatic delete(s).
-
-  // 3. std::mutex tail_mutex is used whenever tail is accessed. Mutex is locked
-  // either manually or is locked by our condition variable
-
-  // 4. node* tail is the pointer to tail. Note we cannot have tail as
-  // unique_ptr as that would make two unique_ptr(s) to tail (one through)
-  // linked list and one through our decalaration. Thus we make this a normal
-  // pointer and this pointer is safely deallocated using the linked list
-  // unique_ptr during call to destructor
-
-  // 5. condition_variable cond is used to check whether queue is empty or not
-  // and do a blocking wait on
-
-  // Private member functions :
-  // node *get_tail() : Helper function to get normal pointer to tail at a
-  // particular instant std::unique_ptr wait_and_get() : Helper function to
-  // blocking wait on unique_ptr of head after popping std::unique_ptr try_get()
-  // : Helper function to try to get unique_ptr of head after popping
+  // Private helper functions (defined in impl.hpp)
+  node *get_tail();
+  std::unique_ptr<node> wait_and_get();
+  std::unique_ptr<node> try_get();
 
 public:
-  // Public member functions :
-  // Add relevant constructors and destructors -> Add these here only
-  // 1. void push(value) : Pushes the value inside the queue, copies the value
-  // 2. void wait_and_pop(value ref) : Blocking wait on queue, returns value in
-  // the reference passed as parameter
-  // 3. std::shared_ptr wait_and_pop(void) : Blocking wait on queue, returns
-  // value as a shared ptr allocated inside the call
-  // 4. bool try_pop(value ref) : Returns true and gives the value in reference
-  // passed, false otherwise
-  // 5. std::shared_ptr try_pop() : Returns a shared ptr with data, returns
-  // nullptr if failed
-  // 6. bool empty() : Returns whether the queue is empty or not at that instant
-  // 7. Add static asserts
-  // 8. Add emplace_back using perfect forwarding and variadic templates (you
-  // can use this in push then)
-  // 9. Add size() function
-  // 10. Any more suggestions ??
+  // Constructor
+  blocking_mpmc_unbounded();
+
+  // No copying allowed (two queues can't share the same linked list)
+  blocking_mpmc_unbounded(const blocking_mpmc_unbounded &) = delete;
+  blocking_mpmc_unbounded &operator=(const blocking_mpmc_unbounded &) = delete;
+
+  // Public member functions (defined in impl.hpp)
+  void push(T value);
+
+  template <typename... Args>
+  void emplace(Args &&...args);
+
+  void wait_and_pop(T &ref);
+  std::shared_ptr<T> wait_and_pop();
+  bool try_pop(T &ref);
+  std::shared_ptr<T> try_pop();
+  bool empty();
+  size_t size();
 };
 } // namespace tsfqueue::__impl
+
+// Include the out-of-line template definitions
+#include "impl.hpp"
 
 #endif
--- include/blocking_mpmc_unbounded/impl.hpp
+++ include/blocking_mpmc_unbounded/impl.hpp
@@ -6,24 +6,115 @@
 template <typename T>
 using queue = tsfqueue::__impl::blocking_mpmc_unbounded<T>;
 
-template <typename T> void queue<T>::push(T value) {}
-
-template <typename T> queue<T>::node *queue<T>::get_tail() {}
-
+// Constructor
 template <typename T>
-std::unique_ptr<queue<T>::node> queue<T>::wait_and_get() {}
+queue<T>::blocking_mpmc_unbounded()
+    : head(std::make_unique<node>()), tail(head.get()) {}
 
-template <typename T> std::unique_ptr<queue<T>::node> queue<T>::try_get() {}
+// Private: get_tail
+template <typename T>
+typename queue<T>::node *queue<T>::get_tail() {
+    std::lock_guard<std::mutex> lock(tail_mutex);
+    return tail;
+}
 
-template <typename T> void queue<T>::wait_and_pop(T &value) {}
+// Private: wait_and_get (blocking)
+template <typename T>
+std::unique_ptr<typename queue<T>::node> queue<T>::wait_and_get() {
+    std::unique_lock<std::mutex> lock(head_mutex);
+    cond.wait(lock, [this]() { return head.get() != get_tail(); });
+    std::unique_ptr<node> old_head = std::move(head);
+    head = std::move(old_head->next);
+    return old_head;
+}
 
-template <typename T> std::shared_ptr<T> queue<T>::wait_and_pop() {}
+// Private: try_get (non-blocking)
+template <typename T>
+std::unique_ptr<typename queue<T>::node> queue<T>::try_get() {
+    std::unique_lock<std::mutex> lock(head_mutex);
+    if (head.get() == get_tail()) {
+        return nullptr;
+    }
+    std::unique_ptr<node> old_head = std::move(head);
+    head = std::move(old_head->next);
+    return old_head;
+}
 
-template <typename T> bool queue<T>::try_pop(T &value) {}
+// Public: push
+template <typename T>
+void queue<T>::push(T value) {
+    std::shared_ptr<T> new_data = std::make_shared<T>(std::move(value));
+    std::lock_guard<std::mutex> lock(tail_mutex);
+    tail->data = new_data;
+    tail->next = std::make_unique<node>();
+    tail = tail->next.get();
+    cond.notify_one();
+}
 
-template <typename T> std::shared_ptr<T> queue<T>::try_pop() {}
+// Public: emplace (construct T in-place using perfect forwarding)
+template <typename T>
+template <typename... Args>
+void queue<T>::emplace(Args &&...args) {
+    std::shared_ptr<T> new_data = std::make_shared<T>(std::forward<Args>(args)...);
+    std::lock_guard<std::mutex> lock(tail_mutex);
+    tail->data = new_data;
+    tail->next = std::make_unique<node>();
+    tail = tail->next.get();
+    cond.notify_one();
+}
 
-template <typename T> bool queue<T>::empty() {}
+// Public: wait_and_pop (reference version)
+template <typename T>
+void queue<T>::wait_and_pop(T &ref) {
+    std::unique_ptr<node> old_head = wait_and_get();
+    ref = std::move(*(old_head->data));
+}
+
+// Public: wait_and_pop (shared_ptr version)
+template <typename T>
+std::shared_ptr<T> queue<T>::wait_and_pop() {
+    std::unique_ptr<node> old_head = wait_and_get();
+    return old_head->data;
+}   
+
+// Public: try_pop (reference version)
+template <typename T>
+bool queue<T>::try_pop(T &ref) {
+    std::unique_ptr<node> const old_head = try_get();
+    if (!old_head) {
+        return false;
+    }
+    ref = std::move(*(old_head->data));
+    return true;
+}
+
+// Public: try_pop (shared_ptr version)
+template <typename T>
+std::shared_ptr<T> queue<T>::try_pop() {
+    std::unique_ptr<node> const old_head = try_get();
+    if (!old_head) {
+        return nullptr;
+    }
+    return old_head->data;
+}
+
+// Public: empty
+template <typename T>
+bool queue<T>::empty() {
+    std::lock_guard<std::mutex> lock(head_mutex);
+    return head.get() == get_tail();
+}
+
+// Public: size
+template <typename T>
+size_t queue<T>::size() {
+    std::lock_guard<std::mutex> lock(head_mutex);
+    size_t count = 0;
+    for (node *temp = head.get(); temp != get_tail(); temp = temp->next.get()) {
+        count++;
+    }
+    return count;
+}
 
 #endif
 
